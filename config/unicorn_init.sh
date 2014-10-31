a33c89eb541ffb0b18cb97a8de4c931154e37202
Hardik Gondaliya <hardikg23@gmail.com>
2014-10-31 15:15:32 +0530

    did : username shiva

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,10 +12,10 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/user/current
+APP_ROOT=/home/shiva/apps/user/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
-AS_USER=deployer
+AS_USER=shiva
 set -u
 
 OLD_PIN="$PID.oldbin"
