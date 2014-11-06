1941a567c2f76069c50fb39075e986c1686f8c7a
Hardik Gondaliya <hardikg23@gmail.com>
2014-11-06 11:29:17 +0530

    did : last commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030094408) do
+ActiveRecord::Schema.define(version: 20141105113148) do
 
   create_table "addresses", force: true do |t|
     t.integer  "pin"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20141030094408) do
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_tmp"
   end
 
   create_table "users", force: true do |t|
